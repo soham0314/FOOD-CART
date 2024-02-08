d6782a0e26e89f3468342c02c770eb3525b8e548
soham0314 <soham141103@gmail.com>
2024-02-08 12:46:22 +0530

    second commit

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// root
+//root
 const heading = React.createElement(
   "h1",
   { id: "heading" },
   "Namaste React 🚀"
 );
 
-ReactDOM.render(heading, document.getElementById("root"));
+console.log(heading);
+
+//jsx - html-like
+
+const jsxHeading = <h1 id="heading">Hello Pune !! Kase aha Mandali</h1>;
+
+console.log(jsxHeading);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(jsxHeading);
---
+++ readme.md
@@ -0,0 +1,8 @@
+# react
+
+# parcel
+
+- Dev Build
+- local server
+- HMR (hot module replacement)
+- parcel using file watching algorithm which is written in c++
