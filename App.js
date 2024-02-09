7c47e4c958ece6b490181cbca7e8a2adfc76864d
soham0314 <soham141103@gmail.com>
2024-02-09 20:00:45 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//root
+/*root   [8/02/24]
 const heading = React.createElement(
   "h1",
   { id: "heading" },
@@ -10,12 +10,34 @@ const heading = React.createElement(
 
 console.log(heading);
 
-//jsx - html-like
 
-const jsxHeading = <h1 id="heading">Hello Pune !! Kase aha Mandali</h1>;
+// jsx - (html-like) :
+
+const jsxHeading = (
+  <h1 className="head" tabIndex="1">
+    Hello Pune !! Kase aha Mandali🙂
+  </h1>
+);
 
 console.log(jsxHeading);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(jsxHeading);
+*/
+
+// ReactComponent      [9/02/24]
+
+const Title = () => <h1>Helloo everyone!!</h1>;
+
+const HeadingComponent = () => (
+  <div id="container">
+    <Title />
+    <h1 className="heading">HELLO REACT COMPONENTS</h1>
+  </div>
+);
+
+console.log(HeadingComponent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<HeadingComponent />);
