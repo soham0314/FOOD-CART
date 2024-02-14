631921761dbb6058f2a921789a34130337bfb5a6
soham0314 <soham141103@gmail.com>
2024-02-14 17:24:43 +0530

    second app commit

--- App.js
+++ App.js
@@ -1,15 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+//import Header from "./components/Header";
+//import Body from "./components/Body";
 
-const AppLayout = () => {
-  return <div class="app"></div>;
-};
 const Header = () => {
   return (
     <div className="Header">
-      <div>
-        <lmg src="assests\appLogo.png" alt="Try again" />
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"
+          alt="try again"
+        />
+      </div>
+      <div className="navigation-bar">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
       </div>
     </div>
   );
 };
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<AppLayout />);
